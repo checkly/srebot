120c2cbd302bd2f9161d984b5e94d591d10f936f
schobele <leoschoberwalter@gmail.com>
2024-11-15 15:45:52 +0100

    feat: prisma setup

--- src/app/api/v1/checkly/alertreceiver/route.ts
+++ src/app/api/v1/checkly/alertreceiver/route.ts
@@ -9,6 +9,8 @@ import {
 	generateContextAnalysisSummary,
 } from "src/aggregator/chains";
 import { WebhookAlertDto } from "src/checkly/alertDTO";
+import { prisma } from "src/prisma";
+import { Prisma } from "@prisma/client";
 
 export async function GET() {
 	return NextResponse.json({ message: "Hello from Next.js!" });
@@ -31,6 +33,18 @@ export async function POST(request: NextRequest) {
 
 		// TODO: Save the alert to the database
 		console.log("Context analysis summary:", summary);
+
+		await prisma.alert
+			.create({
+				data: {
+					data: { ...alertDto } as unknown as Prisma.InputJsonValue,
+					context: JSON.stringify(contextAnalysis),
+					summary,
+				},
+			})
+			.catch((error) => {
+				console.error("Error saving alert to the database:", error);
+			});
 
 		return NextResponse.json({ message: "OK" });
 	} catch (error) {
