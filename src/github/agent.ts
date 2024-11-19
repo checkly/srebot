2db29956ba61d08c3ecdc50857587d3757e28bb0
Andreas Lüdeke <andreas@luedeke.me>
2024-11-19 10:50:09 +0100

    github release summaries

--- src/github/agent.ts
+++ src/github/agent.ts
@@ -1,135 +1,69 @@
-// // Your imports go here
-// import {
-//     OpenAI,
-//     Ollama,
-//     FunctionTool,
-//     ReActAgent,
-//     Settings,
-//     BaseToolWithCall,
-//     Groq,
-//   } from 'llamaindex';
-//   import 'dotenv/config';
-//   import axios from 'axios';
-//   import { Octokit } from '@octokit/rest';
-//   const groqapikey = process.env.GROQ_API_KEY;
-//   const lokiApiKey = process.env.LOKI_API_KEY!;
-//   const user = process.env.LOKI_USER!;
-//   const lokiUrl = process.env.LOKI_URL!;
-//   export class LokiAgent {
-//     private readonly LokiClient: LokiClient = new LokiClient(
-//       lokiUrl,
-//       lokiApiKey,
-//       user,
-//     );
-//     agent: ReActAgent;
-//     init() {
-//       /*Settings.llm = new Ollama({
-//         model: 'llama3.1:8b-instruct-fp16',
-//         //model: 'llama3.1',
-//       });*/
-//       /*const o = new OpenAI({
-//         //model: 'gpt-3.5-turbo',
-//         model: 'gpt-4o',
-//         //additionalChatOptions: { response_format: { type: 'json_object' } },
-//         apiKey: process.env.OPENAI_API_KEY,
-//       });
-//       o.lazySession();
-//       Settings.llm = o;*/
-  
-//       Settings.llm = new Groq({
-//         apiKey: groqapikey,
-//         model: 'llama3-70b-8192',
-//       });
-  
-//       Settings.callbackManager.on('llm-tool-call', (event) => {
-//         console.log(event.detail);
-//       });
-//       Settings.callbackManager.on('llm-tool-result', (event) => {
-//         console.log(event.detail);
-//       });
-//       const tools = this.setupTools();
-//       this.agent = new ReActAgent({ tools: tools, verbose: true });
-//     }
-  
-//     setupTools(): BaseToolWithCall[] {
-//       const getServices = FunctionTool.from(
-//         this.LokiClient.getAvailableServices,
-//         {
-//           name: 'getServices',
-//           description:
-//             'use this function to get a list of available services you can query logs for',
-//           parameters: {
-//             type: [],
-//             properties: {},
-//             required: [],
-//           },
-//         },
-//       );
-//       const getErrors = FunctionTool.from(
-//         async ({
-//           serviceName,
-//           minutes,
-//         }: {
-//           serviceName: string;
-//           minutes: number;
-//         }): Promise<string> => {
-//           if (!serviceName || !minutes) {
-//             return 'you need to provide a service name and minutes to query back';
-//           }
-//           const c = await this.LokiClient.getErrorsForService(
-//             serviceName,
-//             minutes,
-//           );
-//           if (c.data.result.length === 0) {
-//             return 'No errors found';
-//           }
-//           return c.data.result[0].values;
-//         },
-//         {
-//           name: 'getErrors',
-//           description:
-//             'use this function to get all errors for a service within a specified time range',
-//           parameters: {
-//             type: 'object',
-//             properties: {
-//               serviceName: {
-//                 type: 'string',
-//                 description: 'the name of the service you want to query',
-//               },
-//               minutes: {
-//                 type: 'number',
-//                 description: 'the amount of minutes you want to query back',
-//               },
-//             },
-//             required: ['serviceName', 'minutes'],
-//           },
-//         },
-//       );
-//       return [getErrors, getServices];
-//     }
-  
-//     async queryAgent(question: string) {
-//       console.log(`querying agent `);
-//       const response = await this.agent.chat({
-//         message: `
-//      call each tool only once. In your final response be  detailed.
-//      Answer in this format:
-//       Findings: your findings and a summary of the logs
-//       Explanation of Errors: explain errors you found
-//       Suggested Fix: what you suggest to fix the errors
-//      Here is the task: 
-//           ${question}
-  
-//            `,
-//       });
-//       console.log(response);
-//       const content = response.message.content;
-//       if (typeof content === 'string') {
-//         // Extract Thought
-  
-//         return { thought: content, answer: content };
-//       } else {
-//         return { thought: 'none', answer: content };
-//       }
-//     }
-//   }
+import { z } from 'zod';
+import { generateText, LanguageModelV1, tool } from 'ai';
+import GitHubAPI from './github';
+
+export class GithubAgent {
+  private model: LanguageModelV1;
+  private github: GitHubAPI;
+
+  constructor(model: LanguageModelV1, github: GitHubAPI) {
+    this.model = model;
+    this.github = github;
+  }
+
+  async summarizeRelease(org: string, repo: string, release: string, previousRelease: string) {
+    let diff = await this.github.getDiffBetweenTags(org, repo, release, previousRelease);
+    console.log("release summary", org, repo, release, previousRelease, JSON.stringify(diff).length);
+
+    const { text } = await generateText({
+      model: this.model,
+      prompt: `The following diff describes the changes between ${previousRelease} and ${release}. Summarize the changes so that another developer quickly understands what has changes: ${JSON.stringify(diff)}. Do not describe the outer context as the developer is already aware. Do not yap.`
+    });
+     
+    return text;
+  }
+
+  async find_repo(org: string, prompt: string) {
+    let repositories = (await this.github.queryRepositories(org)).map(r => ({name: r.name, description: r.description}));
+    console.log('repositories', org, JSON.stringify(repositories));
+
+    const { text } = await generateText({
+      model: this.model,
+      system: `A developer describes a task which is about a repository in his github organization. Select the repository he is most likely talking about and give me only its name. This list of posible repos is the following: ${JSON.stringify(repositories)}`,
+      prompt,
+    });
+     
+    return text;
+  }
+
+  async get_date(org: string, prompt: string) {
+    const { text } = await generateText({
+      model: this.model,
+      system: `A developer describes a task which is about a certain time frame. Based on his prompt choose the correct date. If you cannot find a timeframe return the date from 24h ago. Today is ${new Date().toISOString()}`,
+      prompt,
+    });
+     
+    return text;
+  }
+
+  async summarizeReleases(prompt: string, org: string) {
+    let repo = await this.find_repo(org, prompt);
+    console.log('repo', org, repo);
+    let since = await this.get_date(org, prompt);
+    console.log('since', org, since);
+    let releases = await this.github.queryLatestReleases(org, repo, new Date(since));
+    console.log('releases', org, repo, JSON.stringify(releases).length);
+
+    return await Promise.all(releases.slice(0, -1).map(async (release, i) => {
+      let previousRelease = releases[i + 1]?.tag || '';
+      let summary = await this.summarizeRelease(org, repo, release.tag, previousRelease);
+      console.log("summary: ", JSON.stringify(summary, null, 2));
+      return {
+        id: release.tag,
+        repo: repo,
+        release_date: release.date,
+        summary: summary
+      };
+    }));     
+  }
+}
