9630c1e1ffe6ecb641858beab9ac813ed1f35f63
Andreas Lüdeke <andreas@luedeke.me>
2024-11-19 14:55:10 +0100

    adding github summaries to slackbot

--- src/github/agent.ts
+++ src/github/agent.ts
@@ -19,11 +19,11 @@ export class GithubAgent {
       prompt: `The following diff describes the changes between ${previousRelease} and ${release}. Summarize the changes so that another developer quickly understands what has changes: ${JSON.stringify(diff)}. Do not describe the outer context as the developer is already aware. Do not yap.`
     });
      
-    return text;
+    return { diff, summary: text };
   }
 
   async find_repo(org: string, prompt: string) {
-    let repositories = (await this.github.queryRepositories(org)).map(r => ({name: r.name, description: r.description}));
+    let repositories = (await this.github.queryRepositories(org)).map(r => ({name: r.name, description: r.description, link: r.html_url}));
 
     const { text } = await generateText({
       model: this.model,
@@ -31,13 +31,13 @@ export class GithubAgent {
       prompt,
     });
      
-    return text;
+    return repositories.find(r => r.name === text) || undefined;
   }
 
   async get_date(org: string, prompt: string) {
     const { text } = await generateText({
       model: this.model,
-      system: `A developer describes a task which is about a certain time frame. Based on his prompt choose the correct date. If you cannot find a timeframe return the date from 24h ago. Today is ${new Date().toISOString()}`,
+      system: `A developer describes a task which is about a certain time frame. Based on his prompt choose identify the date in ISO8601 format. If you cannot find a timeframe return the date from 24h ago. Today is ${new Date().toISOString()}. Do not yap.`,
       prompt,
     });
      
@@ -46,18 +46,30 @@ export class GithubAgent {
 
   async summarizeReleases(prompt: string, org: string) {
     let repo = await this.find_repo(org, prompt);
-    let since = await this.get_date(org, prompt);
-    let releases = await this.github.queryLatestReleases(org, repo, new Date(since));
+    if (repo === undefined) {
+      throw new Error('Could not find repository');
+    }
 
-    return await Promise.all(releases.slice(0, -1).map(async (release, i) => {
+    let since = await this.get_date(org, prompt);
+    let releases = await this.github.queryLatestReleases(org, repo.name, new Date(since));
+
+    let summaries = await Promise.all(releases.slice(0, -1).map(async (release, i) => {
       let previousRelease = releases[i + 1]?.tag || '';
-      let summary = await this.summarizeRelease(org, repo, release.tag, previousRelease);
+      let { diff, summary} = await this.summarizeRelease(org, repo.name, release.tag, previousRelease);
+      console.log(JSON.stringify(diff, undefined, 2));
       return {
         id: release.tag,
-        repo: repo,
         release_date: release.date,
-        summary: summary
+        link: release.link,
+        summary: summary,
+        authors: Array.from(new Set(diff.commits.map(c => c.author))),
       };
-    }));     
+    }));
+    
+    return {
+      repo: repo,
+      since: since,
+      releases: summaries
+    }
   }
 }
--- src/slackbot/app.ts
+++ src/slackbot/app.ts
@@ -1,4 +1,8 @@
+import { createOpenAI } from '@ai-sdk/openai';
 import { App, LogLevel } from '@slack/bolt';
+import { GithubAgent } from '../github/agent';
+import GitHubAPI from '../github/github';
+import moment from 'moment';
 
 export const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
@@ -34,8 +38,22 @@ app.message('whatismyuserid', async ({ context, say }) => {
   await say(context.userId!);
 });
 
+let setupAgent = () => {
+  const OPENAI_API_KEY = process.env.OPENAI_API_KEY!;
+  const CHECKLY_GITHUB_TOKEN = process.env.CHECKLY_GITHUB_TOKEN!;
+
+  let openai = createOpenAI({ apiKey: OPENAI_API_KEY });
+  let github = new GitHubAPI(CHECKLY_GITHUB_TOKEN);
+
+  return new GithubAgent(openai('gpt-4o'), github);
+}
+
+const githubAgent = setupAgent();
+
 app.event('app_mention', async ({ event, context }) => {
+  console.log('app_mention event:', event);
   try {
+    
     //needs reactions:write
     /*await app.client.reactions.add({
       token: context.botToken,
@@ -43,22 +61,37 @@ app.event('app_mention', async ({ event, context }) => {
       channel: event.channel,
       timestamp: event.ts,
     });*/
-    const initialResponse = await app.client.chat.postMessage({
-      token: context.botToken,
-      channel: event.channel,
-      //<@${event.user}>
-      text: `Hello ! Let me think..`,
-    });
+    // const initialResponse = await app.client.chat.postMessage({
+    //   token: context.botToken,
+    //   channel: event.channel,
+    //   //<@${event.user}>
+    //   text: `Hello ! Let me think..`,
+    // });
     const updateUser = async (msg: string) => {
       app.client.chat.postMessage({
         token: context.botToken,
         channel: event.channel,
         //<@${event.user}>
         text: msg,
-        thread_ts: initialResponse.ts,
+        thread_ts: event.ts,
       });
     };
-    await updateUser('wait for my response dude');
+
+    await updateUser(`I am thinking...`);
+
+    let summaries = await githubAgent.summarizeReleases(event.text, 'checkly');
+    if (summaries.releases.length === 0) {
+      await updateUser(`No releases found in repo ${summaries.repo} since ${summaries.since}`);
+    }
+
+    let releases = summaries.releases.sort((a, b) => new Date(b.release_date).getTime() - new Date(a.release_date).getTime());
+    let response = releases.map(summary => {
+      const formattedDate = moment(summary.release_date).fromNow();
+      return `<${summary.link}|*${summary.id}*>\n:date: ${formattedDate}\n:package: <${summaries.repo.link}|${summaries.repo.name}>\n\`\`\`${summary.summary}\`\`\`\n Authors: ${summary.authors.map(author => author?.name).join(', ')}`;
+    }).reduce((prev, curr) => {
+      return prev + '\n' + curr;
+    });
+    await updateUser(response);
     //await agent.queryAgent(event.text, updateUser);
   } catch (error) {
     console.error('Error reacting to mention:', error);
