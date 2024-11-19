557bd75058f298cf91a68831b9dfc44268538a26
schobele <leoschoberwalter@gmail.com>
2024-11-19 16:03:55 +0100

    feat: enhance webhook route to create alerts and post messages to Slack

--- src/routes/checklywebhook.ts
+++ src/routes/checklywebhook.ts
@@ -1,55 +1,100 @@
-import express, { Request, Response, NextFunction } from 'express';
-import { plainToInstance } from 'class-transformer';
-import { validateOrReject } from 'class-validator';
-import 'reflect-metadata';
-import { CheckContextAggregator } from '../aggregator/ContextAggregator';
+import express, { Request, Response, NextFunction } from "express";
+import { plainToInstance } from "class-transformer";
+import { validateOrReject } from "class-validator";
+import "reflect-metadata";
+import { CheckContextAggregator } from "../aggregator/ContextAggregator";
 import {
-  generateContextAnalysis,
-  generateContextAnalysisSummary,
-} from '../aggregator/chains';
-import { WebhookAlertDto } from '../checkly/alertDTO';
-import { prisma } from '../prisma';
-import { Prisma } from '@prisma/client';
+	generateContextAnalysis,
+	generateContextAnalysisSummary,
+} from "../aggregator/chains";
+import { WebhookAlertDto } from "../checkly/alertDTO";
+import { prisma } from "../prisma";
+import { Prisma } from "@prisma/client";
+import { app } from "../slackbot/app";
+import { getOpenaiClient } from "../ai/openai";
 
 const router = express.Router();
 
-router.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'Hello from Express!' });
+router.get("/", (req: Request, res: Response) => {
+	res.json({ message: "Hello from Express!" });
 });
 
-router.post('/', async (req: Request, res: Response) => {
-  try {
-    const body = req.body;
-    const alertDto = plainToInstance(WebhookAlertDto, body, {
-      enableImplicitConversion: true,
-    });
+router.post("/", async (req: Request, res: Response) => {
+	try {
+		const body = req.body;
+		const alertDto = plainToInstance(WebhookAlertDto, body, {
+			enableImplicitConversion: true,
+		});
 
-    // Validate the DTO
-    await validateOrReject(alertDto);
+		// Validate the DTO
+		await validateOrReject(alertDto);
 
-    const aggregator = new CheckContextAggregator(alertDto);
-    const context = await aggregator.aggregate();
-    const contextAnalysis = await generateContextAnalysis(context);
-    const summary = await generateContextAnalysisSummary(contextAnalysis);
+		const aggregator = new CheckContextAggregator(alertDto);
+		const context = await aggregator.aggregate();
+		const contextAnalysis = await generateContextAnalysis(context);
+		const summary = await generateContextAnalysisSummary(contextAnalysis);
 
-    await prisma.alert
-      .create({
-        data: {
-          data: { ...alertDto } as unknown as Prisma.InputJsonValue,
-          context: JSON.stringify(contextAnalysis),
-          summary,
-        },
-      })
-      .catch((error) => {
-        console.error('Error saving alert to the database:', error);
-        return res.status(500).json({ message: 'Error saving alert to the database' });
-      });
+		const alert = await prisma.alert.create({
+			data: {
+				data: { ...alertDto } as unknown as Prisma.InputJsonValue,
+				context: JSON.stringify(contextAnalysis),
+				summary,
+			},
+		});
 
-    res.json({ message: 'OK' });
-  } catch (error) {
-    console.error('Error parsing or validating request body:', error);
-    res.status(400).json({ message: 'Invalid request body' });
-  }
+		const thread = await getOpenaiClient().beta.threads.create({
+			messages: [
+				{
+					role: "assistant",
+					content: summary,
+				},
+			],
+		});
+		await app.client.chat.postMessage({
+			channel: "C07V9GNU9L6",
+			metadata: {
+				event_type: "alert",
+				event_payload: {
+					alertId: alert.id,
+					threadId: thread.id,
+				},
+			},
+			blocks: [
+				{
+					type: "header",
+					text: {
+						type: "plain_text",
+						text: "🚨 New Alert",
+						emoji: true,
+					},
+				},
+				{
+					type: "divider",
+				},
+				{
+					type: "section",
+					text: {
+						type: "mrkdwn",
+						text: `*Summary*\n${summary}`,
+					},
+				},
+				{
+					type: "context",
+					elements: [
+						{
+							type: "mrkdwn",
+							text: `🕐 Alert created at: ${new Date().toLocaleString()}`,
+						},
+					],
+				},
+			],
+		});
+
+		res.json({ message: "OK" });
+	} catch (error) {
+		console.error("Error parsing or validating request body:", error);
+		res.status(400).json({ message: "Invalid request body" });
+	}
 });
 
 export default router;
