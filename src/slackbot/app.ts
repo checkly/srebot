9090934efc7fe1ae5824160e365d1997860da4a0
schobele <leoschoberwalter@gmail.com>
2024-11-19 16:08:20 +0100

    feat: enhance app mention handling and add sre-assistant

--- src/slackbot/app.ts
+++ src/slackbot/app.ts
@@ -1,75 +1,105 @@
-import { App, LogLevel } from '@slack/bolt';
+import { App, LogLevel } from "@slack/bolt";
+import { getOpenaiClient } from "../ai/openai";
+import { getRunMessages } from "../ai/utils";
+import { SreAssistant } from "../sre-assistant/SreAssistant";
 
 export const app = new App({
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  token: process.env.SLACK_AUTH_TOKEN,
-  appToken: process.env.SLACK_APP_TOKEN,
-  socketMode: true,
-  logLevel:
-    process.env.NODE_ENV !== 'production' ? LogLevel.DEBUG : LogLevel.INFO,
+	signingSecret: process.env.SLACK_SIGNING_SECRET,
+	token: process.env.SLACK_AUTH_TOKEN,
+	appToken: process.env.SLACK_APP_TOKEN,
+	socketMode: true,
+	logLevel:
+		process.env.NODE_ENV !== "production" ? LogLevel.DEBUG : LogLevel.INFO,
 });
 
-app.command('/help123', async ({ command, ack }) => {
-  await ack();
-  await app.client.chat.postEphemeral({
-    channel: command.channel_id,
-    text: 'hey',
-    user: command.user_id,
-  });
+app.command("/help123", async ({ command, ack }) => {
+	await ack();
+	await app.client.chat.postEphemeral({
+		channel: command.channel_id,
+		text: "hey",
+		user: command.user_id,
+	});
 });
 
 app.message(`hey help`, async ({ message, context }) => {
-  await app.client.chat.postEphemeral({
-    channel: message.channel,
-    text: 'e',
-    user: context.userId!,
-  });
+	await app.client.chat.postEphemeral({
+		channel: message.channel,
+		text: "e",
+		user: context.userId!,
+	});
 });
 
-app.message('Hey SREBot', async ({ say }) => {
-  await say('helloworld');
+app.message("Hey SREBot", async ({ say }) => {
+	await say("helloworld");
 });
 
-app.message('whatismyuserid', async ({ context, say }) => {
-  await say(context.userId!);
+app.message("whatismyuserid", async ({ context, say }) => {
+	await say(context.userId!);
 });
 
-app.event('app_mention', async ({ event, context }) => {
-  try {
-    //needs reactions:write
-    /*await app.client.reactions.add({
-      token: context.botToken,
-      name: 'wave',
-      channel: event.channel,
-      timestamp: event.ts,
-    });*/
-    const initialResponse = await app.client.chat.postMessage({
-      token: context.botToken,
-      channel: event.channel,
-      //<@${event.user}>
-      text: `Hello ! Let me think..`,
-    });
-    const updateUser = async (msg: string) => {
-      app.client.chat.postMessage({
-        token: context.botToken,
-        channel: event.channel,
-        //<@${event.user}>
-        text: msg,
-        thread_ts: initialResponse.ts,
-      });
-    };
-    await updateUser('wait for my response dude');
-    //await agent.queryAgent(event.text, updateUser);
-  } catch (error) {
-    console.error('Error reacting to mention:', error);
-  }
-});
+app.event("app_mention", async ({ event, context }) => {
+	try {
+		let threadId;
+		let alertId = "test";
 
-app.message(
-  /^(see you later|im out|heading out|goodbye|have a good one|bye).*/,
-  async ({ context, say }) => {
-    const greetings = ['See you later', 'Have a great one', 'Ciao'];
-    const choice = Math.floor(Math.random() * greetings.length);
-    await say(`${greetings[choice]} <@${context.user}>`);
-  },
-);
+		if ((event as any).thread_ts) {
+			try {
+				const result = await app.client.conversations.replies({
+					channel: event.channel,
+					ts: (event as any).thread_ts,
+					limit: 1,
+					include_all_metadata: true,
+				});
+				if (result.messages && result.messages.length > 0) {
+					const metadata = result.messages[0].metadata?.event_payload as {
+						threadId: string;
+						alertId: string;
+					};
+
+					threadId = metadata?.threadId;
+					alertId = metadata?.alertId;
+				}
+			} catch (error) {
+				console.error("Error fetching parent message:", error);
+			}
+		}
+
+		if (!threadId) {
+			const thread = await getOpenaiClient().beta.threads.create();
+			threadId = thread.id;
+		}
+
+		const assistant = new SreAssistant(threadId, alertId);
+		const userMessage = await assistant.addMessage(event.text);
+		const responseMessages = await assistant
+			.runSync()
+			.then((run) => getRunMessages(threadId, run.id));
+
+		const send = async (msg: string) => {
+			app.client.chat.postMessage({
+				token: context.botToken,
+				channel: event.channel,
+				blocks: [
+					{
+						type: "section",
+						text: {
+							type: "mrkdwn",
+							text: msg,
+						},
+					},
+				],
+				thread_ts: (event as any).thread_ts || event.ts,
+			});
+		};
+
+		await responseMessages.map((msg) =>
+			send(
+				msg.content
+					.map((c) => (c.type === "text" ? c.text.value : ""))
+					.join("\n")
+			)
+		);
+	} catch (error) {
+		console.error("Error reacting to mention:", error);
+	}
+});
