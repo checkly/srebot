7c3e7128d2dfeb56844eaeb6eaa88e2210a2d20c
schobele <leoschoberwalter@gmail.com>
2024-11-22 13:27:59 +0100

    update ChecklyService tests with detailed alert data and validation for context length

--- src/aggregator/checkly-aggregator.spec.ts
+++ src/aggregator/checkly-aggregator.spec.ts
@@ -13,12 +13,32 @@ describe("ChecklyService", () => {
 			plainToInstance(
 				WebhookAlertDto,
 				{
-					CHECK_ID: checks[0].id,
+					CHECK_NAME: "fail50",
+					CHECK_ID: "b68422ae-6528-45a5-85a6-e85e1be9de2e",
+					CHECK_TYPE: "MULTI_STEP",
+					GROUP_NAME: "",
+					ALERT_TITLE: "fail50 has failed",
+					ALERT_TYPE: "ALERT_FAILURE",
+					CHECK_RESULT_ID: "64f3fe90-db20-4817-abce-c3fb9dd4228a",
+					RESPONSE_TIME: 1715,
+					API_CHECK_RESPONSE_STATUS_CODE: 0,
+					"API_CHECK_RESPONSE_STATUS_T∑EXT": "",
+					RUN_LOCATION: "Frankfurt",
+					RESULT_LINK:
+						"https://app.checklyhq.com/checks/b68422ae-6528-45a5-85a6-e85e1be9de2e/results/multi_step/64f3fe90-db20-4817-abce-c3fb9dd4228a",
+					SSL_DAYS_REMAINING: 0,
+					SSL_CHECK_DOMAIN: "",
+					STARTED_AT: "2024-11-15T13:39:26.259Z",
+					TAGS: [],
+					$RANDOM_NUMBER: 3022,
+					$UUID: "cbe2286f-a353-400c-a797-87f4cda1d6d8",
+					moment: "November 15, 2024",
 				},
 				{ enableImplicitConversion: true }
 			)
 		);
 
 		expect(context).toBeDefined();
+		expect(context.length).toBeGreaterThan(0);
 	});
 });
