409e4fd507df5ae38fee311cd3d202b662ff5029
schobele <leoschoberwalter@gmail.com>
2024-11-22 13:16:36 +0100

    add context mapping to drop unnecessary fields

--- src/aggregator/checkly-aggregator.ts
+++ src/aggregator/checkly-aggregator.ts
@@ -1,12 +1,53 @@
 import { CheckContext, ContextKey } from "./ContextAggregator";
 import { checkly } from "../checkly/client";
 import { WebhookAlertDto } from "../checkly/alertDTO";
+import { Check, CheckResult } from "../checkly/models";
+
+const getCheckLogs = async (checkId: string, checkResultId: string) => {
+	const logs = await checkly.getCheckResult(checkId, checkResultId);
+	console.log("logs");
+	console.log(logs);
+
+	return logs;
+};
+
+const mapCheckToContextValue = (check: Check) => {
+	return {
+		checkId: check.id,
+		type: check.checkType,
+		frequency: check.frequency,
+		frequencyOffset: check.frequencyOffset,
+		shouldFail: check.shouldFail,
+		locations: check.locations,
+		tags: check.tags,
+		maxResponseTime: check.maxResponseTime,
+		sslCheckDomain: check.sslCheckDomain,
+		retryStrategy: check.retryStrategy,
+	};
+};
+
+const mapCheckResultToContextValue = (result: CheckResult) => {
+	return {
+		resultId: result.id,
+		hasErrors: result.hasErrors,
+		hasFailures: result.hasFailures,
+		runLocation: result.runLocation,
+		startedAt: result.startedAt,
+		stoppedAt: result.stoppedAt,
+		responseTime: result.responseTime,
+		checkId: result.checkId,
+		attempts: result.attempts,
+		isDegraded: result.isDegraded,
+		overMaxResponseTime: result.overMaxResponseTime,
+		resultType: result.resultType,
+	};
+};
 
 export const checklyAggregator = {
 	fetchContext: async (alert: WebhookAlertDto): Promise<CheckContext[]> => {
 		const [check, results] = await Promise.all([
 			checkly.getCheck(alert.CHECK_ID),
-			checkly.getCheckResults(alert.CHECK_ID, undefined, 1),
+			checkly.getCheckResult(alert.CHECK_ID, alert.CHECK_RESULT_ID),
 		]);
 		const makeCheckContext = (key: ContextKey, value: unknown) => {
 			return {
@@ -17,10 +58,17 @@ export const checklyAggregator = {
 			} as CheckContext;
 		};
 
+		const logs = results.getLog();
+		const script = check.script;
+
 		const checklyCheckContext = [
-			makeCheckContext(ContextKey.ChecklyAlert, { ...alert }),
-			makeCheckContext(ContextKey.ChecklyCheck, check),
-			makeCheckContext(ContextKey.ChecklyResults, results),
+			makeCheckContext(ContextKey.ChecklyScript, script),
+			makeCheckContext(ContextKey.ChecklyCheck, mapCheckToContextValue(check)),
+			makeCheckContext(
+				ContextKey.ChecklyResults,
+				mapCheckResultToContextValue(results)
+			),
+			makeCheckContext(ContextKey.ChecklyLogs, logs),
 		] as CheckContext[];
 
 		return checklyCheckContext;
