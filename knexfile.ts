9b6bda78ef8b0f5dee87c3423a79a12281758dc0
Jan Grzesik <jmgrzesik@gmail.com>
2025-03-04 17:01:44 +0100

    Update knexfile to work well outside of migrations

--- knexfile.ts
+++ knexfile.ts
@@ -1,24 +1,34 @@
-import dotenv from "dotenv";
+import { initConfig } from "./src/lib/init-config";
 
-dotenv.config();
+initConfig();
 
 const dbUrlEnv = process.env.DATABASE_URL;
+if (!dbUrlEnv) {
+  throw new Error("DATABASE_URL is not set in environment variables.");
+}
 
 const config = {
   local: {
-    client: "postgresql",
+    client: "pg",
     connection: dbUrlEnv,
+    pool: { min: 2, max: 10 },
+    debug: true,
   },
   dev: {
-    client: "postgresql",
+    client: "pg",
     connection: dbUrlEnv,
+    pool: { min: 2, max: 10 },
+    debug: true,
   },
   production: {
-    client: "postgresql",
+    client: "pg",
     connection: dbUrlEnv,
+    pool: { min: 2, max: 20 },
   },
 };
 
 const currentConfig = config[process.env.NODE_ENV || "local"];
+
+console.log(`msg="Loading Knex config" env=${process.env.NODE_ENV || "local"}`);
 
 export default currentConfig;
