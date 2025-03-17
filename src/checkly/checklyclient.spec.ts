1ec871dc607812f2ab8787be4ad235a25cc9dab4
Andreas Lüdeke <andreas@luedeke.me>
2025-03-17 10:38:02 +0100

    Update ChecklyService tests to use environment variable for account ID and skip certain tests for better clarity.

--- src/checkly/checklyclient.spec.ts
+++ src/checkly/checklyclient.spec.ts
@@ -62,9 +62,7 @@ describe("ChecklyService", () => {
   });
 
   it.skip("can retrieve account", async () => {
-    const result = await client.getAccount(
-      "04fec768-8ac4-4a9c-ae72-5522045772f5",
-    );
+    const result = await client.getAccount(process.env.CHECKLY_ACCOUNT_ID!);
 
     expect(result).toBeDefined();
   });
@@ -82,8 +80,10 @@ describe("ChecklyService", () => {
     expect(result).toBeDefined();
   });
 
-  it("can run a check", async () => {
-    const result = await client.runCheck(process.env.CHECKLY_ACCOUNT_ID!);
+  it.skip("can run a check", async () => {
+    const result = await client.runCheck(
+      "e7608d1a-c013-4194-9da0-dec05d2fbabc",
+    );
 
     expect(result).toBeDefined();
   });
