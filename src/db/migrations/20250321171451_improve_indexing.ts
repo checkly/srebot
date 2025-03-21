86b3d7d21bf21f0171e2ad2dc944d3425cf52c89
Jan Grzesik <jmgrzesik@gmail.com>
2025-03-21 18:51:56 +0100

    Remove unnecessary schema change

--- src/db/migrations/20250321171451_improve_indexing.ts
+++ src/db/migrations/20250321171451_improve_indexing.ts
@@ -5,14 +5,15 @@ const INDEX_STARTED_AT_CHECK_ID_ACCOUNT_ID =
 const INDEX_CHECK_RESULTS_CHECKID = "check_results_checkid_index";
 const INDEX_IDX_CHECK_RESULTS_LATEST = "idx_check_results_latest";
 const INDEX_CHECK_RESULTS_QUERY = "check_results_query_index";
-const INDEX_STARTED_AT_RUN_LOCATION_CHECK_ID =
-  "check_results_aggregation_index";
 
 export async function up(knex: Knex): Promise<void> {
+  // This is more optimal because startedAt is the first column in the index
+  // It can be used in all types of queries
   await knex.schema.raw(
     `CREATE INDEX IF NOT EXISTS ${INDEX_STARTED_AT_CHECK_ID_ACCOUNT_ID} ON check_results ("startedAt", "checkId", "accountId")`,
   );
 
+  // Drop unnecessary indexes
   await knex.schema.raw(`DROP INDEX IF EXISTS ${INDEX_CHECK_RESULTS_CHECKID}`);
   await knex.schema.raw(
     `DROP INDEX IF EXISTS ${INDEX_IDX_CHECK_RESULTS_LATEST}`,
@@ -25,6 +26,7 @@ export async function down(knex: Knex): Promise<void> {
     `DROP INDEX IF EXISTS ${INDEX_STARTED_AT_CHECK_ID_ACCOUNT_ID}`,
   );
 
+  // Recreate the dropped indexes
   await knex.schema.raw(
     `CREATE INDEX IF NOT EXISTS ${INDEX_CHECK_RESULTS_CHECKID} ON public.check_results ("checkId")`,
   );
@@ -33,8 +35,5 @@ export async function down(knex: Knex): Promise<void> {
   );
   await knex.schema.raw(
     `CREATE INDEX IF NOT EXISTS ${INDEX_CHECK_RESULTS_QUERY} ON public.check_results ("accountId" ASC, "checkId" ASC, "startedAt" DESC)`,
-  );
-  await knex.schema.raw(
-    `CREATE INDEX IF NOT EXISTS ${INDEX_STARTED_AT_RUN_LOCATION_CHECK_ID} ON public.check_results ("checkId" ASC, "runLocation" ASC, "startedAt" DESC)`,
   );
 }
